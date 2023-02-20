5069b0a2fd19e1ddc952a59746220fb26f55900e
Liam Diprose <liam@liamdiprose.com>
2023-02-20 17:37:04 +1300

    Experimentally try to put library in default load place

--- default.nix
+++ default.nix
@@ -24,7 +24,10 @@ buildMix {
     toml
   ];
 
-  # postInstall = ''
-  #   cp ${nativePkg}/lib/libexplorer.so $out/lib/libexplorer.so
-  # '';
+  postInstall = ''
+    ls -l $out/lib/erlang/lib/
+    RUSTLER_LOAD_DIR=$out/lib/erlang/lib/explorer-0.5.5/priv/native
+    mkdir -p $RUSTLER_LOAD_DIR
+    cp ${nativePkg}/lib/libexplorer.so $RUSTLER_LOAD_DIR
+  '';
 }
